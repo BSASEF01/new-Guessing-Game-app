74963ee768a6e8ef4c3947f09b4cb1c181c84555
BSASEF01 <bersabehsolomon16@gmail.com>
2018-03-12 15:03:12 -0400

    Initial Commit

---
+++ new guessing game app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  new guessing game app
+//
+//  Created by Bersabeh Asefa on 3/12/18.
+//  Copyright © 2018 Bersabeh Asefa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
