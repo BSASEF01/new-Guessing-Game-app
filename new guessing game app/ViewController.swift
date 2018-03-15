a1681679111acf487fe3dd0ed20e926fe7c68813
BSASEF01 <bersabehsolomon16@gmail.com>
2018-03-15 15:54:14 -0400

    done the game

--- new guessing game app/ViewController.swift
+++ new guessing game app/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var userFeedBackLabel: UILabel!
     @IBOutlet weak var ruleslabel: UILabel!
    
+    @IBOutlet weak var guessButton: UIButton!
     var randomNumber = Int(arc4random_uniform(100))
     
     var rulesLabelText = """
@@ -24,35 +25,63 @@ class ViewController: UIViewController {
 
 """
     
-    var guesses = 5
-    var userguess = Int()
+ var numberOfGuess = 5
+    
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = #colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1)
-        ruleslabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-     ruleslabel.text = rulesLabelText 
+        
+            ruleslabel.textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+            ruleslabel.text = rulesLabelText
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
-    @IBAction func guessButtonpressed(_ sender: Any) {
+       
    
-        var guess = Int(userGuessField.text!)!
-        if guess < randomNumber {
-            userFeedBackLabel.text = "your guess was low try again"
-        }  else if guess > randomNumber {
-            userFeedBackLabel.text = "your guess was higher try again"
-        }else{
-            userFeedBackLabel.text = "you win🤩"
-}
+    
+    @IBAction func guessButtonPressed(_ sender: Any) {
+        if guessButton.title(for: .normal) == "Play Again"  {
+        } else {
+            guessButton.setTitle("Guess", for: .normal)
+        }
+           if numberOfGuess != 1 {
+            numberOfGuess -= 1
+            
+            var guess = Int(userGuessField.text!)!
+            if guess < randomNumber {
+                userFeedBackLabel.text = "your guess was low try again"
+            }  else if guess > randomNumber {
+                userFeedBackLabel.text = "your guess was higher try again"
+            }else if guess == randomNumber {
+                userFeedBackLabel.text = "you win🤩"
+                view.endEditing(true)
+            }
+        } else  {
+            userFeedBackLabel.text = "you ran out of tries play again"
+                    guessButton.setTitle("Play Again", for: .normal)
+                    randomNumber = Int(arc4random_uniform(100))
+                    numberOfGuess = 5
+            
+        }
+    
+        }
+    }
 
-      }
+
+
+            
+    
+ 
  
 
 
 
-}
+
